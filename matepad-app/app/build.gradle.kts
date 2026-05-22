eb8bbcd180d27afcf9afc64a58a9b0dfaf9e9bbf
c8sk8yb5h5-debug <c8sk8yb5h5@privaterelay.appleid.com>
2026-05-23 01:37:44 +0800

    Add files via upload

--- matepad-app/app/build.gradle.kts
+++ matepad-app/app/build.gradle.kts
@@ -42,6 +42,8 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
 
+    implementation("androidx.security:security-crypto:1.1.0-alpha06")
+
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 }

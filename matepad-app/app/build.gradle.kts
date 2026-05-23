f3e8f4a7aafe6a8abe76797eafb24a119e35a913
c8sk8yb5h5-debug <c8sk8yb5h5@privaterelay.appleid.com>
2026-05-24 01:58:51 +0800

    Bump MatePad version for header scan test

--- matepad-app/app/build.gradle.kts
+++ matepad-app/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.postbird.frame"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.3.3"
+        versionCode = 8
+        versionName = "1.3.4"
     }
 
     buildFeatures {

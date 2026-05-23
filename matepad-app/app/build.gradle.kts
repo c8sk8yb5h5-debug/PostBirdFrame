23327e4817b9817a6d6f33f0e9d66fbe4cbc0fe3
c8sk8yb5h5-debug <c8sk8yb5h5@privaterelay.appleid.com>
2026-05-24 02:03:43 +0800

    Bump MatePad version for update channel test

--- matepad-app/app/build.gradle.kts
+++ matepad-app/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.postbird.frame"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "1.3.4"
+        versionCode = 9
+        versionName = "1.3.5"
     }
 
     buildFeatures {

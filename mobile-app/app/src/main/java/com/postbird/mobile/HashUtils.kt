c32b8588871aedc9485f4169ca0022ae5f71d4aa
c8sk8yb5h5-debug <c8sk8yb5h5@privaterelay.appleid.com>
2026-05-25 00:03:33 +0800

    Add mobile hash utility

---
+++ mobile-app/app/src/main/java/com/postbird/mobile/HashUtils.kt
@@ -0,0 +1,19 @@
+package com.postbird.mobile
+
+import java.io.File
+import java.security.MessageDigest
+
+object HashUtils {
+    fun sha256(file: File): String {
+        val digest = MessageDigest.getInstance("SHA-256")
+        file.inputStream().use { input ->
+            val buffer = ByteArray(8192)
+            while (true) {
+                val read = input.read(buffer)
+                if (read <= 0) break
+                digest.update(buffer, 0, read)
+            }
+        }
+        return digest.digest().joinToString("") { "%02x".format(it) }
+    }
+}

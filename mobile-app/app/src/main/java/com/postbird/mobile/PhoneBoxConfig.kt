b66f600f2b2b29d64f949ce8a5881b8a98a1a7d2
c8sk8yb5h5-debug <c8sk8yb5h5@privaterelay.appleid.com>
2026-05-25 00:56:28 +0800

    Add phone mailbox config

---
+++ mobile-app/app/src/main/java/com/postbird/mobile/PhoneBoxConfig.kt
@@ -0,0 +1,9 @@
+package com.postbird.mobile
+
+object PhoneBoxConfig {
+    const val PROTOCOL = "im" + "aps"
+    const val HOST = "imap." + "qq.com"
+    const val PORT = 993
+    const val FOLDER = "IN" + "BOX"
+    const val SCAN_LIMIT = 30
+}

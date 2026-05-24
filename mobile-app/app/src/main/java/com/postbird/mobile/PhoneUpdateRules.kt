90c8ad8db2b79d07ce35b4f18db2dcacd62b50fd
c8sk8yb5h5-debug <c8sk8yb5h5@privaterelay.appleid.com>
2026-05-25 00:45:57 +0800

    Add phone update rules

---
+++ mobile-app/app/src/main/java/com/postbird/mobile/PhoneUpdateRules.kt
@@ -0,0 +1,10 @@
+package com.postbird.mobile
+
+object PhoneUpdateRules {
+    const val CURRENT_VERSION_CODE = 3
+    const val CURRENT_VERSION_NAME = "0.3.0"
+    const val APPLICATION_ID = "com.postbird.mobile"
+    const val SUBJECT_KEY = "PostBirdFrame Phone Update"
+    const val ATTACHMENT_KEY = "PostBird-Phone"
+    const val PLATFORM = "phone"
+}

bfbcb32e5bb5f9c28f38d1ef2c5f15f64c3c51e4
c8sk8yb5h5-debug <c8sk8yb5h5@privaterelay.appleid.com>
2026-05-25 00:20:50 +0800

    Bump MatePad to 1.3.10

--- matepad-app/app/build.gradle.kts
+++ matepad-app/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.postbird.frame"
         minSdk = 26
         targetSdk = 35
-        versionCode = 13
-        versionName = "1.3.9"
+        versionCode = 14
+        versionName = "1.3.10"
     }
 
     buildFeatures {

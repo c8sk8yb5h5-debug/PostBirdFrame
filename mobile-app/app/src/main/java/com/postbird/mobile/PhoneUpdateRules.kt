7d135af6ca5b54b2ddb2e67ad2b92e933a0c7846
c8sk8yb5h5-debug <c8sk8yb5h5@privaterelay.appleid.com>
2026-05-25 01:16:33 +0800

    Bump phone update rules to 0.4.0

--- mobile-app/app/src/main/java/com/postbird/mobile/PhoneUpdateRules.kt
+++ mobile-app/app/src/main/java/com/postbird/mobile/PhoneUpdateRules.kt
@@ -1,8 +1,8 @@
 package com.postbird.mobile
 
 object PhoneUpdateRules {
-    const val CURRENT_VERSION_CODE = 3
-    const val CURRENT_VERSION_NAME = "0.3.0"
+    const val CURRENT_VERSION_CODE = 4
+    const val CURRENT_VERSION_NAME = "0.4.0"
     const val APPLICATION_ID = "com.postbird.mobile"
     const val SUBJECT_KEY = "PostBirdFrame Phone Update"
     const val ATTACHMENT_KEY = "PostBird-Phone"

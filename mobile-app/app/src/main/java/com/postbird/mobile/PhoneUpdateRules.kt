c073b50140afb86bc351396110ec150bd178eab0
c8sk8yb5h5-debug <c8sk8yb5h5@privaterelay.appleid.com>
2026-05-25 13:05:29 +0800

    Remove hardcoded phone current version constants

--- mobile-app/app/src/main/java/com/postbird/mobile/PhoneUpdateRules.kt
+++ mobile-app/app/src/main/java/com/postbird/mobile/PhoneUpdateRules.kt
@@ -1,8 +1,6 @@
 package com.postbird.mobile
 
 object PhoneUpdateRules {
-    const val CURRENT_VERSION_CODE = 4
-    const val CURRENT_VERSION_NAME = "0.4.0"
     const val APPLICATION_ID = "com.postbird.mobile"
     const val SUBJECT_KEY = "PostBirdFrame Phone Update"
     const val ATTACHMENT_KEY = "PostBird-Phone"

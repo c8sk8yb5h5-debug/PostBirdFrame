cc66c013354a74bd60af9dcc3449cf920248069d
c8sk8yb5h5-debug <c8sk8yb5h5@privaterelay.appleid.com>
2026-05-25 14:07:46 +0800

    Switch MatePad home to V5 inline update card

--- matepad-app/app/src/main/java/com/postbird/frame/matepad/MainActivity.kt
+++ matepad-app/app/src/main/java/com/postbird/frame/matepad/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PostBirdFrameTheme {
-                MatePadPreviewHomeScreenV4()
+                MatePadPreviewHomeScreenV5()
             }
         }
     }
